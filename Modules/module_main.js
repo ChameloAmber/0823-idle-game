9b774f8c61cb7c967831deb8d7cde02550912309
ChameloAmber <chameloamber@gmail.com>
2023-08-04 04:39:03 +0700

    [v001.02] Testing a bunch of loading methods

--- Modules/module_main.js
+++ Modules/module_main.js
@@ -9,6 +9,40 @@ function gameTick() {
 }
 
 //$('#pageGear').load('Modules/Gear/page_gear.html');
-$('#pageGear').load('testLoad.html');
+// Test Load method #1
+$('#testLoadPageNumber1').load('testLoad.html');
+
+// Test Load method #2
+let xhttp;
+let element = document.getElementById('testLoadPageNumber2');
+let file = 'testLoad2.html';
+
+if (file) {
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            if (this.readyState == 200) {element.innerHTML = this.responseText;}
+            if (this.readyState == 404) {element.innerHTML = "<h1>Page not found</h1>";}
+        }
+    }
+    xhttp.open("GET", file, true);
+    xhttp.send();
+}
+
+// Test Load method #3
+const targetEl = document.querySelector('#testLoadPageNumber3');
+const loadSnippet = number => {
+    fetch(`./testLoad3.html`)
+        .then(res => {
+            if(res.ok) {
+                return res.text();
+            }
+        })
+        .then(htmlSnippet => {
+            targetEl.innerHTML = htmlSnippet;
+        });
+};
+
+loadSnippet(1);
 
 setInterval(gameTick, 50);
